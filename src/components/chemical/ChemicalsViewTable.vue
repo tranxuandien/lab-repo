0c8d17bd256e892beaa1808d0116a4db35c00bbd
Dien <trangia93vnbk@gmail.com>
2024-09-23 17:12:40 +0700

    update ui

--- src/components/chemical/ChemicalsViewTable.vue
+++ src/components/chemical/ChemicalsViewTable.vue
@@ -1,63 +1,40 @@
 <template>
     <div class="container mt-6">
-        <table class="table table-bordered table-striped table-hover">
-            <thead class="thead-dark">
-                <tr>
-                    <th>Tên hóa chất</th>
-                    <th>Hãng sản xuất</th>
-                    <th>Dạng hóa chất</th>
-                    <th>Khối lượng đăng kí</th>
-                    <th>Khối lượng còn lại</th>
-                    <th>Mô tả đóng gói</th>
-                    <th>Phân loại hóa chất</th>
-                    <th>Phân loại hóa chất</th>
-                    <!-- <th>Thông tin người nhập</th> -->
-                    <th>Vị trí</th>
-                    <th>Hạn dụng</th>
-                    <!-- <th>Tình trạng đăng kí</th> -->
-                    <th>Tình trạng hóa chất</th>
-                    <!-- <th>Nguồn</th> -->
-                    <th>Khác</th>
-                    <th v-if="hasPermission('USER')">Cập nhật</th>
-                    <th v-if="hasPermission('ADMIN')">Xóa</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(chemical, index) in chemicals" :key="index">
-                    <td>{{ chemical.name }}</td>
-                    <td>{{ chemical.brand }}</td>
-                    <td>{{ chemical.chemicalType }}</td>
-                    <td>{{ chemical.manufactoryQuantity }}</td>
-                    <td>{{ chemical.remain }}</td>
-                    <td>{{ chemical.chemicalTypeInfo }}</td>
-                    <td>{{ chemical.chemicalClass }}</td>
-                    <td>{{ chemical.chemicalClassInfo }}</td>
-                    <!-- <td>{{ chemical.registerUser }}</td> -->
-                    <td>{{ chemical.position }}</td>
-                    <td>{{ chemical.expiredDate }}</td>
-                    <!-- <td>{{ chemical.chemicalStatus }}</td> -->
-                    <td>{{ chemical.impExpInfo }}</td>
-                    <!-- <td>{{ chemical.purchaseSrc }}</td> -->
-                    <td>{{ chemical.otherInfo }}</td>
-                    <td v-if="hasPermission('USER')">
-                        <button class="btn btn-primary btn-sm">Cập nhật</button>
-                    </td>
-                    <td v-if="hasPermission('ADMIN')">
-                        <button class="btn btn-danger btn-sm">Xóa</button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        
+
+
+        <div class="card">
+            <DataTable :value="chemicals" paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]"
+                tableStyle="min-width: 50rem" paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+                currentPageReportTemplate="{first} to {last} of {totalRecords}">
+                <Column field="name" header="Name" style="width: 25%"></Column>
+                <Column field="brand" header="Country" style="width: 25%"></Column>
+                <Column field="chemicalType" header="Company" style="width: 25%"></Column>
+                <Column field="manufactoryQuantity" header="Representative" style="width: 25%"></Column>
+            </DataTable>
+        </div>
+
+
     </div>
 </template>
 
 <script>
+// import TablePagination from '@/components/TablePagination.vue';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
 // import api from '@/plugin/axios';
 // import { ref } from 'vue'
 export default {
+    components: {
+        DataTable,
+        Column
+    },
     props: ['chemicals'],
     data() {
         return {
+            perPage: 10,
+            currentPage: 1,
+            rows: this.chemicals?.length,
             mutableList: JSON.parse(this.chemicals)
         };
     },
--- src/main.js
+++ src/main.js
@@ -4,25 +4,30 @@ import Router from './router/index'
 import Vueform from '@vueform/vueform'
 import vueformConfig from './vueform.config'
 import { createApp } from 'vue'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import '@/assets/main.css'
 import Vue3Toastify from 'vue3-toastify';
 import 'vue3-toastify/dist/index.css';
 import { createPinia } from 'pinia'
- 
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 
-
- 
 const app = createApp(App);
 app.use(Router);
-app.use(Vueform,vueformConfig);
+app.use(Vueform, vueformConfig);
 app.use(Vue3Toastify,
-    {
-      autoClose: 3000,
-      // ...
-    })
+  {
+    autoClose: 3000,
+    // ...
+  })
 app.use(createPinia())
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+});
 app.config.productionTip = false
 // app.config.compilerOptions.isCustomElement = (tag) => ['person-details', 'Vueform'].cludes(tag);
 app.mount('#app');
